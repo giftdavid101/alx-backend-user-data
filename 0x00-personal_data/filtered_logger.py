bc8c13aeea41bc679d36b61cebf83a28ef00334d
giftdavid101 <giftdavid101@gmail.com>
2024-11-07 14:09:14 +0100

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" """
+
+import re
+from typing import List, Tuple
+
+PII_FIELDS: Tuple[str] = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(
+    fields: List[str], redaction: str,
+    message: str, separator: str
+) -> str:
+    """
+    Filters message by replacing each value in fields with redaction
+    """
+    for key in fields:
+        pattern = r'({0}=)[^{1}]*({1})'.format(key, separator)
+        message = re.sub(pattern, r'\1{}\2'.format(redaction), message)
+    return message
