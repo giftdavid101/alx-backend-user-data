e8f79fa029c6082ca2d2370dba4eba1d236cbba1
giftdavid101 <giftdavid101@gmail.com>
2024-11-19 15:45:43 +0100

    users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,6 +28,17 @@ def home() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+    Register a new user route
+    """
+    email, password = request.form.get('email'), request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": "%s" % email, "message": "user created"})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
