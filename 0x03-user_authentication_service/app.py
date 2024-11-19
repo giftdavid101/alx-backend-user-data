4f8ca82232c71c31f4d4ecce3299f50157aa1560
giftdavid101 <giftdavid101@gmail.com>
2024-11-19 15:31:12 +0100

    get route /

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -14,6 +14,20 @@ AUTH = Auth()
 
 app = Flask(__name__)
 
+@app.errorhandler(403)
+def unauthorized(error) -> str:
+    """ Forbidden error handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.route("/")
+def home() -> str:
+    """
+    Home route
+    """
+    return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
