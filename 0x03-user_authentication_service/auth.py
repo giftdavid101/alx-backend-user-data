6a00800536495fa1e0a9102bd46891268bb06c0f
giftdavid101 <giftdavid101@gmail.com>
2024-11-19 15:18:15 +0100

    auth.register

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,9 +14,24 @@ def _generate_uuid() -> str:
     from uuid import uuid4
     return str(uuid4())
 
-
 def _hash_password(password: str) -> bytes:
     """Encrypts a password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Creates a new user if the email does not exist
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError("User %s already exists" % email)
 
